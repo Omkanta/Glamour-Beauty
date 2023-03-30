5ff693f4c46094b4c2d482dd6b1c07f6ded4265f
omkanta <1omkant1998@gmail.com>
2023-03-31 03:36:27 +0530

    admin page products

--- dusty-hat-6390/src/Pages/Admin_Page.jsx
+++ dusty-hat-6390/src/Pages/Admin_Page.jsx
@@ -1,5 +1,115 @@
-
-
-export const Admin_Page=()=>{
-    return <>Admin Page</>
-}
+import {Text} from '@chakra-ui/react'
+  
+  import { Button,Heading } from '@chakra-ui/react'
+  import { useState ,useEffect} from "react";
+  import React from "react";
+  import axios from "axios";
+  import { Select } from '@chakra-ui/react';
+  import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+    Image,
+  } from '@chakra-ui/react';
+  import { Input } from '@chakra-ui/react'
+  
+  export const Admin_Page=()=>{
+   
+    const [category,setcategory]=useState("")
+  const[data,setData]=useState([])
+  
+  
+  function getData(){
+    if(category){
+      axios.get(`http://localhost:8080/${category}`)
+      .then(function (response) {
+        setData(response.data)
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+     }
+    
+  }
+  useEffect(()=>{
+    getData()
+  },[category])
+  
+    const handleOnchange=(e)=>{
+     let query=e.target.value
+     console.log(query)
+     axios.get(`http://localhost:8080/${category}`, {
+      params: {
+        q: query
+      }
+    })
+    .then(function (response) {
+      setData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  
+    }
+    
+   function handleDelete(id){
+    axios.delete(`http://localhost:8080/${category}/${id}`)
+    .then(function (response) {
+      console.log(response);
+      getData()
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+   }
+   
+    return (
+      <div >
+          <Text fontSize={'2xl'} mt={'10px'}>Add new Product</Text>
+          <Select placeholder='Select Category' w='20%' m='auto' mt='30px' value={category} onChange={(e)=>setcategory(e.target.value)}> 
+    <option value='kajal'>Kajal</option>
+    <option value='lipstick'>Lipstick</option>
+    <option value='facewash'>Facewash</option>
+  </Select>
+          <Input width={500} marginTop="20px" className = "searchAddress"  placeholder = "Search Data" onChange={handleOnchange}/>
+  
+          
+          <TableContainer  whiteSpace="wrap">
+            <Table className="table">
+              <Thead >
+                <Tr>
+                  <Th><Heading as='h4' size='md'>Title</Heading></Th>
+                  <Th><Heading as='h4' size='md'>Image</Heading></Th>
+                  <Th><Heading as='h4' size='md'>Price</Heading></Th>
+                  <Th><Heading as='h4' size='md'>Edit</Heading></Th>
+                  <Th><Heading as='h4' size='md'>Delete Item</Heading></Th>
+                </Tr>
+              </Thead>
+              <Tbody> 
+                {data.map((el)=> (
+                  <Tr key={el.id} className = "houseDetails"  >
+                  {/* <Td  className = "name" >{el.brand}</Td> */}
+                  <Td className = "ownersName" >{el.name}</Td>
+                  <Td  className = "address" ><Image width="30%"  src={el.image}></Image></Td>
+                  <Td className = "areaCode" >Rs {el.price}</Td>
+                  <Td _hover={{cursor:"pointer"}} className = "edit"  ><Button colorScheme="blue">Edit</Button>  </Td>
+                  <Td _hover={{cursor:"pointer"}} className = "delete"  ><Button onClick={()=>handleDelete(el.id)} colorScheme="red">Delete</Button>  </Td>
+                  
+              </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+      </div>
+    )
+  }
+  
+  
+  
+  
--- dusty-hat-6390/src/Redux/store.js
+++ dusty-hat-6390/src/Redux/store.js
@@ -1,11 +1,11 @@
 import {  combineReducers, legacy_createStore, applyMiddleware } from "redux";
 import { reducer as ProductReducer } from "./reducer";
 import thunk from "redux-thunk"
-
+import {reducer as AdminReducer} from "./AdminReducer/reducer"
 
 
 const MainReducer = combineReducers({
-    ProductReducer
+    ProductReducer,AdminReducer
 });
 
 export const store = legacy_createStore(MainReducer,applyMiddleware(thunk));
